5be47212e89bce4a4f725617a4286d1414348b0b
TJ <xxterrelljacksonxx@gmail.com>
2022-10-25 19:44:50 -0400

    first commit

---
+++ src/components/Design.js
@@ -0,0 +1,12 @@
+import './Design.css'
+
+export default function Design() {
+    return (
+        <nav>
+            <div className='container'>
+                <h1>The Design System</h1>
+                <section id="colors" />
+            </div>
+        </nav>
+    )
+}
